ad33cddce9f283c06692127fe21c7c4ac6884af3
Peixoto <lucas.peixoto@nestdigital.com.br>
2020-03-12 10:37:59 -0300

    aspectos ok

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 $( document ).ready(function() {
 
-  $('.boxNPCs').html(
-  '<div class="boxNPC">'+
-    '<center><h4>'+aspects('concept')+'</h4>'+
-    '<p><b><i>'+aspects('good')+', '+aspects('problem')+'</i></b></p></center>'+
-    '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
-    '<p><strong>Ruim (-2) em:</strong> '+skills('skilled')+'</p>'+
-    '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'+
-  '</div>');
-
-  $('.boxNPCs').append(
-  '<div class="boxNPC">'+
-    '<center><h4>'+aspects('concept')+'</h4>'+
-    '<p><b><i>'+aspects('good')+', '+aspects('problem')+'</i></b></p></center>'+
-    '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
-    '<p><strong>Ruim (-2) em:</strong> '+skills('skilled')+'</p>'+
-    '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'+
-  '</div>');
-
-  $('.boxNPC').css({'border': 'solid 1px black', 'border-radius': '5px', 'padding': '5px', 'margin': '10px', 'max-width': '500px', 'box-shadow': '5px 5px 10px lightgrey'});
-
+  setTimeout(function() {
+      $("#btnGerarNPC").trigger('click');
+  },10);
+  
   $('#btnGerarNPC').click(function(){
 
     $('.boxNPCs').html(
