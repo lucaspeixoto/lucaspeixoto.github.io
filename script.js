d800246070f4834377b299b9e0bf255b7109f5f9
Peixoto <lucas.peixoto@nestdigital.com.br>
2020-03-11 22:49:40 -0300

    arquivos ok

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $( document ).ready(function() {
       '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
       '<p><strong>Ruim (-2) em:</strong> '+skills('bad')+'</p>'+
       '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'+
-    '</div>');
+    '</div>'); 
 
   });
 
