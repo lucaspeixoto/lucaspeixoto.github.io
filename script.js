c52b3d032972fa07110fe8b90e954e691b221f13
Peixoto <lucas.peixoto@nestdigital.com.br>
2020-03-11 22:43:27 -0300

    ok

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   $('#btnGerarNPC').click(function(){
 
     $('.boxNPCs').html(
-    '<div>'+
+    '<div class="boxNPC">'+
       '<h3 class="title">Humano</h3>'+
       '<i><h4 class="aspects">'+aspects('good')+', '+aspects('bad')+'</h4></i>'+
       '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
@@ -12,7 +12,7 @@ $( document ).ready(function() {
     '</div>');
 
     $('.boxNPCs').append(
-    '<div>'+
+    '<div class="boxNPC">'+
       '<h3 class="title">Humano</h3>'+
       '<i><h4 class="aspects">'+aspects('good')+', '+aspects('bad')+'</h4></i>'+
       '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
@@ -21,7 +21,7 @@ $( document ).ready(function() {
     '</div>');
 
     $('.boxNPCs').append(
-    '<div>'+
+    '<div class="boxNPC">'+
       '<h3 class="title">Humano</h3>'+
       '<i><h4 class="aspects">'+aspects('good')+', '+aspects('bad')+'</h4></i>'+
       '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
@@ -30,7 +30,7 @@ $( document ).ready(function() {
     '</div>');
 
     $('.boxNPCs').append(
-    '<div>'+
+    '<div class="boxNPC">'+
       '<h3 class="title">Humano</h3>'+
       '<i><h4 class="aspects">'+aspects('good')+', '+aspects('bad')+'</h4></i>'+
       '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
