140cda237e4299946e2184e8c9ea2a35e72d8e1a
Peixoto <lucas.peixoto@nestdigital.com.br>
2020-03-11 23:03:38 -0300

    arquivos ok 5

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ $( document ).ready(function() {
 
     $('.boxNPCs').html(
     '<div class="boxNPC">'+
-      '<h4 class="title">Humano</h4>'+
-      '<p><b><i>'+aspects('good')+', '+aspects('bad')+'</i></b></p>'+
+      '<center><h4>Humano</h4>'+
+      '<p><b><i>'+aspects('good')+', '+aspects('bad')+'</i></b></p></center>'+
       '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
       '<p><strong>Ruim (-2) em:</strong> '+skills('bad')+'</p>'+
       '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'+
@@ -13,32 +13,15 @@ $( document ).ready(function() {
 
     $('.boxNPCs').append(
     '<div class="boxNPC">'+
-      '<h4 class="title">Humano</h4>'+
-      '<p><b><i>'+aspects('good')+', '+aspects('bad')+'</i></b></p>'+
-      '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
-      '<p><strong>Ruim (-2) em:</strong> '+skills('bad')+'</p>'+
-      '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'+
-    '</div>');
-
-    $('.boxNPCs').append(
-    '<div class="boxNPC">'+
-      '<h4 class="title">Humano</h4>'+
-      '<p><b><i>'+aspects('good')+', '+aspects('bad')+'</i></b></p>'+
-      '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
-      '<p><strong>Ruim (-2) em:</strong> '+skills('bad')+'</p>'+
-      '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'+
-    '</div>');
-
-    $('.boxNPCs').append(
-    '<div class="boxNPC">'+
-      '<h4 class="title">Humano</h4>'+
-      '<p><b><i>'+aspects('good')+', '+aspects('bad')+'</i></b></p>'+
+      '<center><h4>Humano</h4>'+
+      '<p><b><i>'+aspects('good')+', '+aspects('bad')+'</i></b></p></center>'+
       '<p><strong>Perito (+2) em:</strong> '+skills('skilled')+', '+skills('skilled')+'</p>'+
       '<p><strong>Ruim (-2) em:</strong> '+skills('bad')+'</p>'+
       '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'+
     '</div>');
 
     $('.boxNPC').css({'border': 'solid 1px black', 'border-radius': '5px', 'padding': '5px', 'margin': '10px', 'max-width': '500px', 'box-shadow': '5px 5px 10px lightgrey'});
+    $('.boxNPC p').css('margin','10px 0px !important');
 
   });
 
