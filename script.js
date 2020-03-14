99b08f47a0ffae44aaf124dee03f5af3c9372006
Peixoto <lucas.peixoto@nestdigital.com.br>
2020-03-14 11:06:58 -0300

    inicio pc

--- script.js
+++ script.js
@@ -35,17 +35,19 @@ $( document ).ready(function() {
 
   $('#btnGerarFate').click(function(){
 
-    var classe = classes();
-    var raca = races();
+    //var classe = classes();
+    //var raca = races();
 
-    var npcSkills = exclusiveRandom(skills('core'),5);
+    var skills = exclusiveRandom(skills('core'),10);
 
     $('.boxPersonagemFate').html(
-      '<p><i>Conceito:</i> '+random(classe.aspects())+' '+random(aspects('ancestry'))+'</p>'+
-      '<p><i>Raça:</i> '+random(raca.aspects())+'</p>'+
-      '<p><i>Problema:</i> '+random(aspects('problem'))+'</p>'+
-      '<p><strong>Perito (+2) em:</strong> '+npcSkills[0]+', '+npcSkills[1]+', '+npcSkills[2]+'</p>'+
-      '<p><strong>Ruim (-2) em:</strong> '+npcSkills[3]+', '+npcSkills[4]+'</p>'+
+    //  '<p><i>Conceito:</i> '+random(classe.aspects())+' '+random(aspects('ancestry'))+'</p>'+
+    //  '<p><i>Raça:</i> '+random(raca.aspects())+'</p>'+
+      '<p><i>Problema:</i> '+random(aspects('problem'))+'</p><br>'+
+      '<p><strong>Ótimo (+4):</strong> '+skills[0]+'</p>'+
+      '<p><strong>Bom (+3):</strong> '+skills[1]+', '+skills[2]+'</p>'+
+      '<p><strong>Razoável (+2):</strong> '+skills[3]+', '+skills[4]+', '+skills[5]+'</p>'+
+      '<p><strong>Regular (+1):</strong> '+skills[6]+', '+skills[7]+', '+skills[8]+', '+skills[9]+'</p>'+
       '<p><strong>Estresse: </strong>'+stress(0,2)+'</p>'
     );   
 
