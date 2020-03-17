093a4382f8af9593a883a1e79859add9161a85d3
Peixoto <lucas.peixoto@nestdigital.com.br>
2020-03-16 23:39:19 -0300

    names ok

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ $( document ).ready(function() {
     facanhasRaca = facanhasRaca.concat(raca[racaEspecifica].stunts);
 
     $('.boxPersonagemFate').html(
-      '<center><h5>'+random(raca[racaEspecifica].names)+'</h5>'+
+      '<center><h4>'+random(raca[racaEspecifica].names)+'</h4></center>'+
       '<p><strong>ASPECTOS</strong><br>'+
       '<strong>&nbsp;&nbsp;&nbsp;Conceito:</strong> '+classeEspecifica+' '+random(aspects('ancestry'))+'<br>'+
       '<strong>&nbsp;&nbsp;&nbsp;Raça:</strong> '+racaEspecifica+'<br>'+
