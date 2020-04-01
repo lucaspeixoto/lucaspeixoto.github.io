1cbd27e6ffd22b9f4796e610654db99b3561fe46
Peixoto <lucas.peixoto@nestdigital.com.br>
2020-04-01 10:16:30 -0300

    stress box

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 $( document ).ready(function() {
 
-  $('.fa-square-o').on( "click", function(){
-    $(this).removeClass('fa-square-o');
-    $(this).addClass('fa-square');
-  });
-
-  $('.fa-square').on( "click", function(){
-    $(this).removeClass('fa-square');
-    $(this).addClass('fa-square-o');
-  });
-
   setTimeout(function() {
       $("#btnGerarNPC").trigger('click');
       $("#btnGerarFate").trigger('click');
@@ -40,6 +30,16 @@ $( document ).ready(function() {
     '</div>');
 
     $('.boxNPC').css({'border': 'solid 1px black', 'border-radius': '5px', 'padding': '10px', 'margin': '10px', 'margin-left': '0px', 'max-width': '550px', 'box-shadow': '5px 5px 10px lightgrey'});
+
+    $('.fa-square-o').on( "click", function(){
+      $(this).removeClass('fa-square-o');
+      $(this).addClass('fa-square');
+    });
+
+    $('.fa-square').on( "click", function(){
+      $(this).removeClass('fa-square');
+      $(this).addClass('fa-square-o');
+    });
 
   });
 
@@ -121,6 +121,16 @@ $( document ).ready(function() {
     );
 
     $('.boxPersonagemFate').css({'border': 'solid 1px black', 'border-radius': '5px', 'padding': '10px', 'margin': '10px', 'margin-left': '0px', 'max-width': '550px', 'box-shadow': '5px 5px 10px lightgrey'});
+
+    $('.fa-square-o').on( "click", function(){
+      $(this).removeClass('fa-square-o');
+      $(this).addClass('fa-square');
+    });
+
+    $('.fa-square').on( "click", function(){
+      $(this).removeClass('fa-square');
+      $(this).addClass('fa-square-o');
+    });
 
   });
 
