26c03a5dfe029e49fce20b71140a9f9245d3d81d
Lucas Peixoto <lucaspeixoto.cco@gmail.com>
2023-09-22 17:24:32 -0300

    gerador busca altaris

---
+++ js/altaris.js
@@ -0,0 +1,139 @@
+$( document ).ready(function() {
+
+  function buscar(){
+    return [
+      'Não encontrou nada. Perca 1 Conquista.',
+      'Encontrou objeto sem valor:',
+      'Encontrou objeto sem valor:',
+      'Encontrou um objeto comum, mas útil:',
+      'Encontrou um equipamento de qualquer raridade:',
+      'Encontrou um Tesouro:'
+    ];
+  }
+
+  function tesouro(){
+    return [
+      'Poção aleatória.',
+      'Objeto valioso ou muitas moedas (1 Riqueza).',
+      'Objeto valioso ou muitas moedas (1 Riqueza).',
+      'Ornamento mágico aleatório.',
+      'Vestimenta mágica aleatória.',
+      'Arma mágica aleatória.',
+    ]; 
+  }
+
+  function pocao(){
+    return ['Poção do Caos ','Poção de Levitação','Poção de Energia ','Poção de Metamorfose','Poção de Energia ','Poção de Invisibilidade','Poção de Energia ','Poção de Foco','Poção de Vida ','Poção de Aura Elemental','Poção de Vida ','Poção Universal'];
+  }
+
+  function ornamento(){
+
+    tipo = random(['Estatueta','Cálice','Busto','Lâmpada','Orbe','Ovo','Varinha','Quadro','Espelho','Caneca','Cubo','Mão Empalhada'])
+    efeito = random(['Vórtex ','Fedor','Prisão ','Falante','Agarrador ','Visão Mágica','Iluminador ','Aura Elemental','Animal ','Restaurador','Portal ','Desejo']);
+
+    return tipo+', com o efeito '+efeito;
+  }
+
+  function magiaArmadura(){
+
+    negativo = random(['Agarrador','Palavra','Limite','Falante','Ambiente','Exaustivo','Dolorido','Emocional','Enfurecedor','Procurado','Espécie','Precioso','Mórbido','Inimigo','Chamativo','Fedor','Moribundo','Marcas'])
+    positivo = random(['Acrescente +2 PV máximo','Acrescente +2 PV máximo','Iluminador','Acrescente +2 PE máximo','Acrescente +2 PE máximo','Invisibilidade','Reduz 2 de dano de eletricidade','Reduz 2 de dano de eletricidade','Focado','Reduz 2 de dano de frio','Reduz 2 de dano de frio','Metamorfose','Reduz 2 de dano de fogo','Reduz 2 de dano de fogo','Visão Mágica','Acrescente +2 PV e +2 PE máx.','Acrescente +2 PV e +2 PE máx.','Aura Elemental']);
+
+    return ' (Efeito negativo: '+negativo+' - Efeito positivo: '+positivo+')';
+  }
+
+  function magiaArma(){
+
+    negativo = random(['Agarrador','Palavra','Limite','Falante','Ambiente','Exaustivo','Dolorido','Emocional','Enfurecedor','Procurado','Espécie','Precioso','Mórbido','Inimigo','Chamativo','Fedor','Moribundo','Marcas'])
+    positivo = random(['Causa +2 de dano','Causa +2 de dano','Iluminador','Causa +3 de crítico','Causa +3 de crítico','Retornável','Causa +3 dano de eletricidade','Causa +3 dano de eletricidade','Dançante','Causa +3 dano de frio','Causa +3 dano de frio','Vampírica','Causa +3 dano de fogo','Causa +3 dano de fogo','Matadora','Causa +5 de dano','Causa +5 de dano','Vorpal']);
+
+    return ' (Efeito negativo: '+negativo+' - Efeito positivo: '+positivo+')';
+  }
+  
+  function objetoSemValor(){
+    return ['Botões soltos','Parafusos enferrujados','Pedras quebradas','Pedaços de vidro','Cacos de cerâmica','Galhos de árvores mortas','Raízes entrelaçadas','Folhas murchas','Conchas vazias e rachadas','Páginas arrancadas de um livro','Fragmentos de couro','Pequenos ossos de animais','Punhados de poeira','Lascas de madeira','Fios de cabelo de animais','Dentes de alho secos','Pedras de seixos sem brilho','Cabelos soltos','Fios de palha','Pregos enferrujados','Galhos secos','Folhas de papel em branco','Pétalas secas de flores','Fragmentos de metal enferrujado','Restos de comida deteriorada','Pano rasgado','Peles de frutas velhas','Cinzas de fogueira apagada','Galhos de arbustos secos','Cones de pinheiro vazios','Sementes comuns','Pelotas de excremento seco','Penas danificadas','Pedrinhas comuns','Pedaços de folha de lata','Retalhos de tecido desbotado','Rolhas usadas','Lã suja','Roupas rasgadas e gastas','Casca de árvore solta','Teias de aranha sem aranhas','Fragmentos de carvão','Pequenas quantidades de serragem','Folhas de outono murchas','Restos de velas queimadas','Corda desgastada','Grama cortada','Areia comum','Pedras pequenas e comuns','Tufos de pelos de roedores','Cabelos humanos cortados','Conchas de moluscos vazias','Fragmentos de conchas quebradas','Restos de ossos de animais','Pequenas lascas de pedra','Gravetos apodrecidos','Tintas ressecadas','Escovas de pintura gastas','Pedras de rio sem brilho','Restos de velhos pergaminhos','Penas de ave comuns','Argolas enferrujadas','Pedaços de barro seco','Vidro moído em pequenos fragmentos','Fragrâncias desbotadas','Rolos de tecido inutilizados','Restos de velhas velas de cera','Galhos de arbustos sem vida','Penas de ganso danificadas','Fios de linho desgastados','Folhas de metal amassadas','Restos de garras de animais','Carvão apagado','Pedra calcária comum','Folhas de papel sujo','Pelotas de argila seca','Cones de pinheiro vazios e quebrados','Cacos de cerâmica sem forma','Galhos de árvores quebrados','Restos de comida mofada','Tecido desfiado','Peles de frutas apodrecidas','Cinzas de fogueira extinta','Galhos de arbustos quebrados','Cones de pinheiro despedaçados','Sementes inúteis','Lã suja e enredada','Roupas desgastadas e rasgadas','Casca de árvore sem utilidade','Teias de aranha antigas e desgastadas','Fragmentos de carvão sem valor','Pequenas quantidades de serragem inútil','Folhas de outono secas sem brilho','Restos de velas queimadas inutilizáveis','Corda frágil','Grama cortada e sem vida','Areia comum e sem valor','Pedras pequenas e sem brilho','Tufos de pelos de roedores inúteis','Cabelos humanos cortados'];
+  }
+
+  function objetoUtil(){
+    return ['Cantil de couro','Tochas','Isqueiro de pedra','Saco de dormir de lã','Mochila de lona','Barras de pão','Saco de sal','Mapa local desgastado','Comida seca (carne defumada, frutas secas)','Pederneira e aço','Pá de madeira','Luvas de couro','Linha e agulha','Bolsa de moedas de cobre','Óleo de lâmpada','Martelo de ferreiro','Pregos de ferro','Estacas de tenda de madeira','Roupas de viagem simples','Giz de ardósia','Saco de carvão vegetal','Sabão caseiro','Kit de primeiros socorros básico','Corda grossa','Saco de giz de cera','Pergaminho e pena','Canivete de osso','Espelho de mão com moldura de madeira','Panela de ferro','Kit de higiene pessoal (escova de dente de javali, sabonete de ervas)','Vela de cera de abelha','Mecha de vela de linho','Saco de especiarias','Fio de seda e tinta','Lápis de carvão','Folhas de papel pergaminho','Pequeno caldeirão de ferro fundido','Selo de cera','Bússola rudimentar','Bandagens de algodão','Máscara de couro','Mapa estelar desenhado à mão','Frasco de vidro vazio','Sacola de ervas secas','Rolos de tecido simples','Corda de sisal','Pinça de cozinha','Rolha de garrafa de cerâmica','Saquinho com seixos','Flauta de osso','Teias de aranha natural','Aranhas em frascos de vidro','Garrafas de vidro vazias','Tubos de vidro vazios','Garrafas de cerâmica decoradas','Roupas desgastadas','Travesseiros de palha','Lenços de pano','Escovas de cabelo de javali','Chaveiro de madeira','Medalhões de madeira com símbolos antigos','Tambores de madeira e pele','Livro antigo','Colheres de madeira entalhadas','Frascos de tinta','Pequenas estátuas de cerâmica','Fivelas de cinto de bronze'];
+  }
+
+  function equipamento(tipo){
+    if (tipo == '')
+      tipo = random(['arma','armadura','outro']);
+
+    switch(tipo){
+      case "arma":
+        return ['Punhal','Sabre Élfico','Katar','Khopesh','Manding','Facão Uruk','Cimitarra','Espada de Cavaleiro','Takouba','Picareta','Martelo Anão','Macuahuitl','Espada Ulfberht','Maça Inca','Mangual','Machado Epsilon','Machado Pêndulo','Machado Berserker','Alabarda Inca','Rede','Tridente de Espinhos','Arco Longo Inglês ','Atlatl','Boleadeira','Arco Mongol','Faca','Adaga','Sabre','Rapieira','Clava','Machado','Maça','Lança Curta','Martelo de Guerra','Espada Longa','Cajado','Montante','Machado Pesado','Lança Longa','Gadanha','Alabarda','Funda','Arco Simples','Arco de Guerra','Arco de Caça','Besta de Mão','Besta Pesada'];
+        break;
+      case "armadura":
+        return ['Cota de Folhas Anciãs','Cota de Talas ','Cota de Malha ','Cota de Ferro ','Armadura de Casco','Armadura Completa','Túnica Árabe ','Cocar ','Turbante do Rei','Elmo Mongol ','Elmo-Lamparina','Escudo Redondo','Broquel','Túnica Confortável ','Roupa Comum ','Armadura de Couro','Cota de Malha ','Peitoral ','Elmo ','Escudo'];
+        break;
+      case "outro":
+        return ['Chifre-Caneco','Astrolábio','Aljava','Kit Acampamento','Kit Escalada ','Instrumento Musical'];
+        break;
+      case "armaduraMagia":
+        return ['Cota de Folhas Anciãs','Cota de Talas','Cota de Malha','Cota de Ferro','Armadura de Casco','Armadura Completa','Túnica Árabe','Cocar','Turbante do Rei','Elmo Mongol','Elmo-Lamparina','Escudo Redondo','Broquel','Túnica Confortável','Roupa Comum','Armadura de Couro','Cota de Malha','Peitoral','Elmo','Escudo','Anel','Botas','Luvas','Máscara','Manto','Coroa','Brincos','Colar','Bracelete'];
+        break;
+    }
+  }
+
+
+  $('#btnGerarBusca').click(function(){
+
+    var item = '';
+    var busca = random(buscar());
+
+    switch(busca){
+      case 'Não encontrou nada. Perca 1 Conquista.':
+        item = '';
+        break;
+      case 'Encontrou objeto sem valor:':
+        item = random(objetoSemValor());
+        break;
+      case 'Encontrou um objeto comum, mas útil:':
+        item = random(objetoUtil());
+        break;
+      case 'Encontrou um equipamento de qualquer raridade:':
+        item = random(equipamento(''));
+        break;
+      case 'Encontrou um Tesouro:':
+        item = random(tesouro());
+
+        switch(item){
+          case 'Poção aleatória.':
+            item = '<i>Poção aleatória</i><br>' + random(pocao());
+            break;
+          case 'Objeto valioso ou muitas moedas (1 Riqueza).':
+            item = 'Objeto valioso ou muitas moedas (1 Riqueza).';
+            break;
+          case 'Ornamento mágico aleatório.':
+            item = '<i>Ornamento mágico aleatório</i><br>'+ornamento();
+            break;
+          case 'Vestimenta mágica aleatória.':
+            item = '<i>Vestimenta mágica aleatória</i><br>'+random(equipamento('armaduraMagia')) + magiaArmadura();
+            break;
+          case 'Arma mágica aleatória.':
+            item = '<i>Arma mágica aleatória</i><br>'+random(equipamento('arma')) + magiaArma();
+            break;
+        }
+
+        break;
+    }
+
+    $('.boxNPCs').html(
+    '<div class="boxNPC">'+
+      '<p><b>'+busca+'</b>'+
+      '<br>'+item+'</p>'+
+    '</div>');
+
+    $('.boxNPC').css({'border': 'solid 1px black', 'border-radius': '5px', 'padding': '10px', 'margin': '10px', 'margin-left': '0px', 'max-width': '550px', 'box-shadow': '5px 5px 10px lightgrey'});
+
+  });
+
+  setTimeout(function() {
+      $("#btnGerarBusca").trigger('click');
+  },10);
+
+});
+
