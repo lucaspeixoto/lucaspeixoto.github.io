1bdde2a60c21567494ea9646be5f0c825e7d954e
Lucas Peixoto <lucaspeixoto.cco@gmail.com>
2026-05-24 19:24:00 -0300

    gerador de personagem

--- js/shadowdark.js
+++ js/shadowdark.js
@@ -1,12 +1,581 @@
 $(document).ready(function() {
+ 
+    function rolarDado(lados) {
+        return Math.floor(Math.random() * lados) + 1;
+    }
 
-function random(lista) {
-    return lista[Math.floor(Math.random() * lista.length)];
-}
+    function rolar(expressao) {
 
-function rolarDado(lados) {
-    return Math.floor(Math.random() * lados) + 1;
-}
+        const partes = expressao.toLowerCase().split('d');
+
+        const quantidade = parseInt(partes[0]);
+        const lados = parseInt(partes[1]);
+
+        let total = 0;
+
+        for (let i = 0; i < quantidade; i++) {
+            total += rolarDado(lados);
+        }
+
+        return total;
+    }
+
+    function random(lista) {
+        return lista[Math.floor(Math.random() * lista.length)];
+    }
+
+    function modificador(valor) {
+
+        if (valor <= 3) return -4;
+        if (valor <= 5) return -3;
+        if (valor <= 7) return -2;
+        if (valor <= 9) return -1;
+        if (valor <= 11) return 0;
+        if (valor <= 13) return 1;
+        if (valor <= 15) return 2;
+        if (valor <= 17) return 3;
+
+        return 4;
+    }
+
+    function textoModificador(mod) {
+
+        if (mod >= 0) {
+            return '+' + mod;
+        }
+
+        return mod;
+    }
+
+    function ancestralidade() {
+
+        const valor = rolarDado(12);
+
+        if (valor <= 4) return 'Humano';
+        if (valor <= 6) return 'Elfo';
+        if (valor <= 8) return 'Anão';
+        if (valor <= 10) return 'Halfling';
+        if (valor == 11) return 'Meio-orc';
+
+        return 'Goblin';
+    }
+
+    function classe() {
+
+        const valor = rolarDado(4);
+
+        switch (valor) {
+
+            case 1:
+                return 'Guerreiro';
+
+            case 2:
+                return 'Ladrão';
+
+            case 3:
+                return 'Mago';
+
+            case 4:
+                return 'Sacerdote';
+
+        }
+
+    }
+
+    function alinhamento() {
+
+        const valor = rolarDado(6);
+
+        if (valor <= 3) return 'Ordeiro';
+        if (valor <= 5) return 'Neutro';
+
+        return 'Caótico';
+    }
+
+    function antecedente() {
+
+        const lista = [
+            'Garoto de Rua',
+            'Procurado',
+            'Iniciado do Culto',
+            'Guilda dos Ladrões',
+            'Banido',
+            'Órfão',
+            'Aprendiz de Mago',
+            'Joalheiro',
+            'Herbalista',
+            'Bárbaro',
+            'Mercenário',
+            'Marinheiro',
+            'Acólito',
+            'Soldado',
+            'Guardião',
+            'Batedor',
+            'Menestrel',
+            'Estudioso',
+            'Nobre',
+            'Cirurgião'
+        ];
+
+        return random(lista);
+    }
+
+    function nome(ancestralidade) {
+
+        const nomes = {
+
+            'Humano': [
+                'Zali', 'Bram', 'Clara', 'Nattias', 'Rina',
+                'Denton', 'Mirena', 'Aran', 'Morgan', 'Giralt',
+                'Tamra', 'Oscar', 'Ishana', 'Rogar', 'Jasmin',
+                'Tarin', 'Yuri', 'Malchor', 'Lienna', 'Godfrey'
+            ],
+
+            'Elfo': [
+                'Eliara', 'Ryarn', 'Sariel', 'Tirolas', 'Galira',
+                'Varos', 'Daeniel', 'Axidor', 'Hiralia', 'Cyrwin',
+                'Lothiel', 'Zaphiel', 'Nayra', 'Ithior', 'Amriel',
+                'Elyon', 'Jirwyn', 'Natinel', 'Fiora', 'Ruhiel'
+            ],
+
+            'Anão': [
+                'Hilde', 'Torbin', 'Marga', 'Bruno', 'Karina',
+                'Naugrim', 'Brenna', 'Darvin', 'Elga', 'Alric',
+                'Isolde', 'Gendry', 'Bruga', 'Junnor', 'Vidrid',
+                'Torson', 'Brielle', 'Ulfgar', 'Sarna', 'Grimm'
+            ],
+
+            'Halfling': [
+                'Willow', 'Benny', 'Annie', 'Tucker', 'Marie',
+                'Hobb', 'Cora', 'Gordie', 'Rose', 'Ardo',
+                'Alma', 'Norbert', 'Jennie', 'Barvin', 'Tilly',
+                'Pike', 'Lydia', 'Marlow', 'Astrid', 'Jasper'
+            ],
+
+            'Goblin': [
+                'Iggs', 'Tark', 'Nix', 'Lenk', 'Roke',
+                'Fitz', 'Tila', 'Riggs', 'Prim', 'Zeb',
+                'Finn', 'Borg', 'Yark', 'Deeg', 'Nibs',
+                'Brak', 'Fink', 'Rizzo', 'Squib', 'Grix'
+            ],
+
+            'Meio-orc': [
+                'Vara', 'Gralk', 'Ranna', 'Korv', 'Zasha',
+                'Hrogar', 'Klara', 'Tragan', 'Brolga', 'Drago',
+                'Yelena', 'Krull', 'Ulara', 'Tulk', 'Shiraal',
+                'Wulf', 'Ivara', 'Hirok', 'Aja', 'Zoraan'
+            ]
+
+        };
+
+        return random(nomes[ancestralidade]);
+    }
+
+    function idiomaComum() {
+
+        const lista = [
+            'Anão',
+            'Élfico',
+            'Gigante',
+            'Goblin',
+            'Merrano',
+            'Orc',
+            'Reptiliano',
+            'Silvestre',
+            'Thaniano'
+        ];
+
+        return random(lista);
+    }
+
+    function idiomaRaro() {
+
+        const lista = [
+            'Celestial',
+            'Diabólico',
+            'Dracônico',
+            'Primordial'
+        ];
+
+        return random(lista);
+    }
+
+    function magiaMago() {
+
+        const lista = [
+            'Alarme',
+            'Armadura Arcana',
+            'Detectar Magia',
+            'Disco Flutuante',
+            'Encantar Pessoa',
+            'Luz',
+            'Mãos Flamejantes',
+            'Míssil Mágico',
+            'Obstruir Porta',
+            'Proteção contra o Mal',
+            'Queda Suave',
+            'Sono'
+        ];
+
+        return random(lista);
+    }
+
+    function magiaSacerdote() {
+
+        const lista = [
+            'Luz',
+            'Curar Ferimentos',
+            'Arma Sagrada',
+            'Proteção contra o Mal',
+            'Escudo da Fé'
+        ];
+
+        return random(lista);
+    }
+
+    function divindade() {
+
+        const lista = [
+            'Santa Terragnis',
+            'Gede',
+            'Madeera',
+            'Ord',
+            'Memnon',
+            'Shune',
+            'Ramlaat'
+        ];
+
+        return random(lista);
+    }
+
+    function tituloClasse(classe, alinhamento) {
+
+        const titulos = {
+
+            'Guerreiro': {
+                'Ordeiro': 'Escudeiro',
+                'Neutro': 'Guerreiro',
+                'Caótico': 'Valete'
+            },
+
+            'Ladrão': {
+                'Ordeiro': 'Malandro',
+                'Neutro': 'Bandoleiro',
+                'Caótico': 'Rufião'
+            },
+
+            'Mago': {
+                'Ordeiro': 'Aprendiz',
+                'Neutro': 'Xamã',
+                'Caótico': 'Adepto'
+            },
+
+            'Sacerdote': {
+                'Ordeiro': 'Acólito',
+                'Neutro': 'Rastreador',
+                'Caótico': 'Iniciado'
+            }
+
+        };
+
+        return titulos[classe][alinhamento];
+    }
+
+    function gerarAtributos() {
+
+        return {
+            forca: rolar('3d6'),
+            destreza: rolar('3d6'),
+            constituicao: rolar('3d6'),
+            inteligencia: rolar('3d6'),
+            sabedoria: rolar('3d6'),
+            carisma: rolar('3d6')
+        };
+
+    }
+
+    function gerarPV(classe, modCon) {
+
+        let base = 0;
+
+        switch (classe) {
+
+            case 'Guerreiro':
+                base = rolarDado(8);
+                break;
+
+            case 'Ladrão':
+                base = rolarDado(6);
+                break;
+
+            case 'Sacerdote':
+                base = rolarDado(6);
+                break;
+
+            case 'Mago':
+                base = rolarDado(4);
+                break;
+
+        }
+
+        return Math.max(1, base + modCon);
+    }
+
+    function gerarCA(classe, modDes) {
+
+        let base = 10 + modDes;
+
+        if (
+            classe == 'Guerreiro' ||
+            classe == 'Ladrão' ||
+            classe == 'Sacerdote'
+        ) {
+            base = 11 + modDes;
+        }
+
+        return base;
+    }
+
+    function gerarArma(classe) {
+
+        const armas = {
+
+            'Guerreiro': [
+                'Espada longa',
+                'Machado grande',
+                'Martelo de guerra',
+                'Arco longo',
+                'Lança'
+            ],
+
+            'Ladrão': [
+                'Adaga',
+                'Espada curta',
+                'Arco curto',
+                'Besta'
+            ],
+
+            'Mago': [
+                'Adaga',
+                'Cajado'
+            ],
+
+            'Sacerdote': [
+                'Maça',
+                'Clava',
+                'Martelo de guerra',
+                'Cajado',
+                'Espada longa'
+            ]
+
+        };
+
+        return random(armas[classe]);
+    }
+
+    function gerarIdiomas(ancestralidade, classe) {
+
+        let idiomas = ['Comum'];
+
+        switch (ancestralidade) {
+
+            case 'Anão':
+                idiomas.push('Anão');
+                break;
+
+            case 'Elfo':
+                idiomas.push('Élfico');
+                break;
+
+            case 'Goblin':
+                idiomas.push('Goblin');
+                break;
+
+            case 'Meio-orc':
+                idiomas.push('Orc');
+                break;
+
+            case 'Halfling':
+                idiomas.push('Silvestre');
+                break;
+
+        }
+
+        if (classe == 'Mago') {
+
+            idiomas.push(idiomaComum());
+            idiomas.push(idiomaComum());
+
+            idiomas.push(idiomaRaro());
+            idiomas.push(idiomaRaro());
+
+        }
+
+        if (classe == 'Sacerdote') {
+
+            idiomas.push(
+                random([
+                    'Celestial',
+                    'Diabólico',
+                    'Primordial'
+                ])
+            );
+
+        }
+
+        return idiomas;
+    }
+
+    function gerarPersonagem() {
+
+        const ancestralidadePJ = ancestralidade();
+        const classePJ = classe();
+        const alinhamentoPJ = alinhamento();
+
+        const atributos = gerarAtributos();
+
+        const modCon = modificador(atributos.constituicao);
+        const modDes = modificador(atributos.destreza);
+
+        const personagem = {
+
+            nome: nome(ancestralidadePJ),
+            ancestralidade: ancestralidadePJ,
+            classe: classePJ,
+            alinhamento: alinhamentoPJ,
+            titulo: tituloClasse(classePJ, alinhamentoPJ),
+
+            nivel: 1,
+
+            antecedente: antecedente(),
+
+            forca: atributos.forca,
+            destreza: atributos.destreza,
+            constituicao: atributos.constituicao,
+            inteligencia: atributos.inteligencia,
+            sabedoria: atributos.sabedoria,
+            carisma: atributos.carisma,
+
+            pv: gerarPV(classePJ, modCon),
+
+            ca: gerarCA(classePJ, modDes),
+
+            ouro: rolar('2d6') * 5,
+
+            arma: gerarArma(classePJ),
+
+            idiomas: gerarIdiomas(
+                ancestralidadePJ,
+                classePJ
+            ),
+
+            equipamento: [
+                'Kit de exploração',
+                '5 PO'
+            ]
+
+        };
+
+        if (
+            classePJ == 'Guerreiro' ||
+            classePJ == 'Ladrão' ||
+            classePJ == 'Sacerdote'
+        ) {
+            personagem.equipamento.push(
+                'Armadura de couro'
+            );
+        }
+
+        if (classePJ == 'Mago') {
+
+            personagem.magias = [
+                magiaMago(),
+                magiaMago(),
+                magiaMago()
+            ];
+
+        }
+
+        if (classePJ == 'Sacerdote') {
+
+            personagem.divindade = divindade();
+
+            personagem.magias = [
+                magiaSacerdote(),
+                magiaSacerdote()
+            ];
+
+        }
+
+        $('.boxNPC').html(`
+
+            <h2>${personagem.nome}</h2>
+
+            <hr>
+
+            <strong>Título:</strong> ${personagem.titulo}<br>
+            <strong>Ancestralidade:</strong> ${personagem.ancestralidade}<br>
+            <strong>Classe:</strong> ${personagem.classe}<br>
+            <strong>Nível:</strong> ${personagem.nivel}<br>
+            <strong>Alinhamento:</strong> ${personagem.alinhamento}<br>
+            <strong>Antecedente:</strong> ${personagem.antecedente}<br>
+
+            <hr>
+
+            <strong>FOR:</strong> ${personagem.forca}
+            (${textoModificador(modificador(personagem.forca))})<br>
+
+            <strong>DES:</strong> ${personagem.destreza}
+            (${textoModificador(modificador(personagem.destreza))})<br>
+
+            <strong>CON:</strong> ${personagem.constituicao}
+            (${textoModificador(modificador(personagem.constituicao))})<br>
+
+            <strong>INT:</strong> ${personagem.inteligencia}
+            (${textoModificador(modificador(personagem.inteligencia))})<br>
+
+            <strong>SAB:</strong> ${personagem.sabedoria}
+            (${textoModificador(modificador(personagem.sabedoria))})<br>
+
+            <strong>CAR:</strong> ${personagem.carisma}
+            (${textoModificador(modificador(personagem.carisma))})<br>
+
+            <hr>
+
+            <strong>PV:</strong> ${personagem.pv}<br>
+            <strong>CA:</strong> ${personagem.ca}<br>
+            <strong>Arma:</strong> ${personagem.arma}<br>
+            <strong>Ouro:</strong> ${personagem.ouro} PO<br>
+
+            <hr>
+
+            <strong>Idiomas:</strong><br>
+            ${personagem.idiomas.join(', ')}
+
+            <hr>
+
+            <strong>Equipamentos:</strong><br>
+            ${personagem.equipamento.join('<br>')}
+
+            ${personagem.divindade ? `
+                <hr>
+                <strong>Divindade:</strong>
+                ${personagem.divindade}
+            ` : ''}
+
+            ${personagem.magias ? `
+                <hr>
+                <strong>Magias:</strong><br>
+                ${personagem.magias.join('<br>')}
+            ` : ''}
+
+        `);
+
+    }
+
+    $('#btnGerarPersonagem').click(function () {
+        gerarPersonagem();
+    });
 
 function ancestralidadeShadowdark(valor) {
 
